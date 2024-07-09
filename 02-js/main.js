febaf1a557d0aaa1a5183130b6035d64a05aeb5b
odd:odd <eehd80yul@gmail.com>
2024-07-09 11:47:09 +0900

    js style

---
+++ 02-js/main.js
@@ -0,0 +1,8 @@
+// function 함수이름() {}
+function myF() {
+    console.log('함수 호출');
+    document.getElementById('text').style.color = 'blue';
+    document.getElementById('text').style.fontSize = '30px';
+    document.getElementById('text').style.backgroundColor = 'yellow';
+    document.getElementById('text').style.transform = 'rotate(45deg) translate(100px, 30px)';
+}
